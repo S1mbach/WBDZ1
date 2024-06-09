075939173a1c0d19cb78f53e05aaf93140fff039
S1mbach <simbatron999@gmail.com>
2024-06-09 18:51:06 +0800

    Initial Commit

---
+++ WB1DZ/WB1DZApp.swift
@@ -0,0 +1,17 @@
+//
+//  WB1DZApp.swift
+//  WB1DZ
+//
+//  Created by Max Sokolov on 09.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WB1DZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
